48f34bc1c81f8ae83b3af0e05b2efe2939ba2992
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2025-07-26 16:13:04 +0300

    add javascript executor and execution mode

--- src/main/java/engine/Bot.java
+++ src/main/java/engine/Bot.java
@@ -2,14 +2,19 @@ package engine;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class Bot {
@@ -17,7 +22,11 @@ public class Bot {
     Wait<WebDriver> wait;
 
     public Bot(){
-        driver = initialize();
+        try {
+            driver = initialize();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
         wait = new FluentWait<>(driver)
                 .withTimeout(Duration.ofSeconds(2))
                 .pollingEvery(Duration.ofMillis(300))
@@ -25,14 +34,27 @@ public class Bot {
         System.out.println("Bot initialized with ChromeDriver and FluentWait.");
     }
 
-    private WebDriver initialize() {
+    private WebDriver initialize() throws MalformedURLException {
         WebDriver driver;
         String browser = System.getProperty("browser", "chrome").toLowerCase();
-        switch (browser) {
-            case "firefox" -> driver = new FirefoxDriver();
-            case "safari" -> driver = new SafariDriver();
-            case "edge" -> driver = new EdgeDriver();
-            default -> driver = new ChromeDriver();
+        String executionMode = System.getProperty("executionMode", "local").toLowerCase();
+        if (executionMode.equals("local")) {
+            switch (browser) {
+                case "firefox" -> driver = new FirefoxDriver();
+                case "safari" -> driver = new SafariDriver();
+                case "edge" -> driver = new EdgeDriver();
+                default -> driver = new ChromeDriver();
+            }
+        } else {
+            switch (browser) {
+                case "firefox" -> driver = new FirefoxDriver();
+                case "safari" -> driver = new SafariDriver();
+                case "edge" -> driver = new EdgeDriver();
+                default -> {
+                    ChromeOptions options = new ChromeOptions();
+                    driver = new RemoteWebDriver(new URL("http://www.example.com"), options);
+                }
+            }
         }
         return driver;
     }
@@ -53,6 +75,15 @@ public class Bot {
             return true;
         });
         System.out.println("Clicked: " + locator);
+    }
+
+    public void scrollUsingJavaScript(By locator, String behavior) {
+        wait.until(d -> {
+            ((JavascriptExecutor) d).executeScript("""
+                 arguments[0].scrollIntoView({behavior: arguments[1], block: "center", inline: "center"});""", d.findElement(locator), behavior);
+            return true;
+        });
+        System.out.println("Clicked using JavaScript: " + locator);
     }
 
     public void type (By locator, CharSequence... text) {
